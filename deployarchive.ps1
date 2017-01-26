00e4e6dd14b9d5f62e30ef515008e9d98ee8f198
Chris Woerz <erendrake@gmail.com>
2017-01-25 22:00:24 -0700

    added support for database artifacts

--- deployarchive.ps1
+++ deployarchive.ps1
@@ -1,16 +1,32 @@
 $build = (Get-Content .\build.json | Out-String | ConvertFrom-Json)
+
+function CreateZip {
+  param( [string] $name ) 
+
+  Add-Type -Assembly System.IO.Compression.FileSystem
+  $compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
+  $sourcedir = $env:BUILD_ARTIFACTSTAGINGDIRECTORY + "\" + $name
+  $zipfilename = $env:BUILD_ARTIFACTSTAGINGDIRECTORY + "\" + $name + ".zip"
+
+  [System.IO.Compression.ZipFile]::CreateFromDirectory($sourcedir, $zipfilename, $compressionLevel, $false)
+  if ($LASTEXITCODE -eq 1)
+  {
+    Write-Host "Error build project $_"
+    exit 1
+  }
+}
+
 if ($build.deploys)
 {
     $build.deploys | ForEach {
-      Add-Type -Assembly System.IO.Compression.FileSystem
-      $compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
-      $sourcedir = $env:BUILD_ARTIFACTSTAGINGDIRECTORY + "\" + $_.name
-      $zipfilename = $env:BUILD_ARTIFACTSTAGINGDIRECTORY + "\" + $_.name + ".zip"
-      [System.IO.Compression.ZipFile]::CreateFromDirectory($sourcedir, $zipfilename, $compressionLevel, $false)
-        if ($LASTEXITCODE -eq 1)
-        {
-            Write-Host "Error build project $_"
-            exit 1
-        }
+        CreateZip $_.name
     }
 }
+
+if ($build.databases)
+{
+    $build.databases | ForEach {
+        CreateZip $_.name
+    }
+}
+
